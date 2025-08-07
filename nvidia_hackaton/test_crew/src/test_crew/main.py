57076debea04380517a8feedf0df6aac3303dd04
Lucas Mantovani Boaro <ra281303@titas.lab.ic.unicamp.br>
2025-08-07 15:00:49 -0300

    criando os agentes

--- nvidia_hackaton/test_crew/src/test_crew/main.py
+++ nvidia_hackaton/test_crew/src/test_crew/main.py
@@ -1,68 +1,18 @@
-#!/usr/bin/env python
-import sys
-import warnings
+from src.analise_acao.crew import create_crew
 
-from datetime import datetime
+def run_analysis(ticker: str, perfil_investidor: str):
+    crew = create_crew()
 
-from test_crew.crew import TestCrew
+    result = crew.kickoff(
+        inputs={
+            "ticker": ticker,
+            "perfil_investidor": perfil_investidor
+        }
+    )
 
-warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
-
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
-def run():
-    """
-    Run the crew.
-    """
-    inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
-    }
-    
-    try:
-        TestCrew().crew().kickoff(inputs=inputs)
-    except Exception as e:
-        raise Exception(f"An error occurred while running the crew: {e}")
+    return result
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
-    try:
-        TestCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        TestCrew().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
-    try:
-        TestCrew().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
+if __name__ == "__main__":
+    markdown_report = run_analysis("VALE3", "moderado")
+    print(markdown_report)
